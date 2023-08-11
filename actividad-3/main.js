09e347fd472d693ac5a819d682ff22622cba1051
Lejito <alejocrrs@gmail.com>
2023-08-10 22:28:02 -0500

    Actividad 3: Ejercicios JS (actualización)

--- actividad-3/main.js
+++ actividad-3/main.js
@@ -6,17 +6,17 @@ function fibonacci(n) {
         return 1
     }
     else {
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2)
     }
 }
 
 
-function palindromo(word) {
+function isPalindrome(word) {
     let wordNormal = word;
     let wordReverse = "";
 
     const len = word.length;
-    for (let i = len-1; i>=0; i--) {
+    for (let i = len - 1; i >= 0; i--) {
         wordReverse += word.charAt(i);
         word = word.slice(0, -1)
     }
@@ -24,18 +24,65 @@ function palindromo(word) {
     return wordNormal === wordReverse;
 }
 
+function findPalindromes(words) {
+    return words.filter((word) => isPalindrome(word));
+}
+
 
 function convertRomanToInt(roman) {
-    const romanDigits = ["I", "V", "X"];
-    const decimalDigits = [1, 5, 10];
-    let romanList = [];
+    const romans = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
 
-    const numberLen = roman.length;
-    for (let i = 0; i < numberLen; i++) {
-        let decimalList = [];
-        let digit = roman.charAt(i);
+    let result = 0;
+    let currentInt = 0;
+    let lastInt = 0;
+    for (let i = roman.length - 1; i >= 0; i--) {
+        currentInt = romans[roman[i]];
 
-        romanDigits.indexOf(digit);
-        //TERMINAR
+        if (currentInt >= lastInt) {
+            result += currentInt;
+        } else {
+            result -= currentInt;
+        }
+
+        lastInt = currentInt;
+    }
+
+    return result;
+}
+
+
+function validatePassword(password) {
+    if (password.length >= 8) {
+        const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*"]
+        let numbersCount = 0;
+        let uppercasesCount = 0;
+        let lowercasesCount = 0;
+        let specialCharactersCount = 0;
+
+        for (let i in password) {
+            let character = password[i];
+
+            if (!isNaN(character)) {
+                numbersCount++;
+            } else if (specialCharacters.includes(character)) {
+                specialCharactersCount++;
+            } else if (character === character.toUpperCase()) {
+                uppercasesCount++;
+            } else if (character === character.toLowerCase()) {
+                lowercasesCount++;
+            }
+        }
+
+        return numbersCount > 0 && uppercasesCount > 0 && lowercasesCount > 0 && specialCharactersCount > 0;
+    } else {
+        return false
     }
 }
